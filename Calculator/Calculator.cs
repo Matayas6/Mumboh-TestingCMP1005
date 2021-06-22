7b439b6b028c57a4de3d66ba8ed1deeb07babaf3
Mathias <38738031+Matayas6@users.noreply.github.com>
2021-06-22 14:41:50 -0400

    Class library

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+
+        public double add(double labelOneInput, double labelTwoInput)
+        {
+            return labelOneInput + labelTwoInput;
+        }
+
+        static public double divide(double labelOneInput, double labelTwoInput)
+        {
+            if (labelTwoInput > 0)
+                return labelOneInput / labelTwoInput;
+
+            throw new DivideByZeroException(@"Dividing by 0 is not expected, change your input");
+        }
+    }
+}
+
--- Mumboh-TestingCMP1005/Pages/Index.cshtml.cs
+++ Mumboh-TestingCMP1005/Pages/Index.cshtml.cs
@@ -12,6 +12,16 @@ namespace Mumboh_TestingCMP1005.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public double Calculation
+        {
+            get; set;
+        }
+
+        public bool CalculationSet
+        {
+            get; protected set;
+        } = false;
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -20,6 +30,31 @@ namespace Mumboh_TestingCMP1005.Pages
         public void OnGet()
         {
 
+        }
+        public void OnPost(double labelOneInput, double labelTwoInput, string logic)
+        {
+
+            switch (logic)
+            {
+                case "add":
+                    Calculation = labelOneInput + labelTwoInput;
+                    CalculationSet = true;
+                    break;
+                case "sub":
+                    Calculation = labelOneInput - labelTwoInput;
+                    CalculationSet = true;
+                    break;
+                case "mul":
+                    Calculation = labelOneInput * labelTwoInput;
+                    CalculationSet = true;
+                    break;
+                case "div":
+                    Calculation = labelOneInput / labelTwoInput;
+                    CalculationSet = true;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
