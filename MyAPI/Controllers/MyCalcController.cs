a8c174e36684aebfd1f90ab74b09c89be1bcf013
Mathias <38738031+Matayas6@users.noreply.github.com>
2021-06-25 18:01:07 -0400

    Complete Assignment

---
+++ MyAPI/Controllers/MyCalcController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculator;
+
+namespace MyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class MyCalcController : ControllerBase
+    {
+        [HttpGet]
+        public double add(double NumberOne, double NumberTwo)
+        {
+            return Calculate.add(NumberOne, NumberTwo);
+        }
+        [HttpGet]
+        public double multiply(double NumberOne, double NumberTwo)
+        {
+            return Calculate.multiply(NumberOne, NumberTwo);
+        }
+        [HttpGet]
+        public double subtract(double NumberOne, double NumberTwo)
+        {
+            return Calculate.subtract(NumberOne, NumberTwo);
+        }
+        [HttpGet]
+        public double divide(double NumberOne, double NumberTwo)
+        {
+            return Calculate.divide(NumberOne, NumberTwo);
+        }
+    }
+}
+
